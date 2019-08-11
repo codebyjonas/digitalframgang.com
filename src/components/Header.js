9658dc3c0caee75467c91cb977f525de17f44d73
Jonas Olaussen <jonasolaussen@gmail.com>
2019-08-11 21:13:54 +0200

    baseline, added burma etc

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => (
+    <>
+        <nav className='navbar' role='navigation' aria-label='main navigation'>
+            <div className='navbar-brand'>
+                <a className='navbar-item'>Tell.se</a>
+            </div>
+        </nav>
+    </>
+)
+
+export default Header
---
+++ src/components/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => (
+    <section className='hero is-primary'>
+        <div className='hero-body'>
+            <div className='container'>
+                <h1 className='title'>Tell.se</h1>
+                <h2 className='subtitle'>Konvertera mera...</h2>
+            </div>
+        </div>
+    </section>
+)
+
+export default Hero
