1097a7f9131fc844161de03c30b33ce008201d0a
Jonas Olaussen <jonasolaussen@gmail.com>
2019-08-12 22:09:20 +0200

    added simple header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,27 @@
 import React from 'react'
 
 const Header = () => (
-    <>
-        <nav className='navbar' role='navigation' aria-label='main navigation'>
-            <div className='navbar-brand'>
-                <a className='navbar-item'>Tell.se</a>
-            </div>
-        </nav>
-    </>
+    <nav className='navbar' role='navigation' aria-label='main navigation'>
+        <div className='navbar-brand'>
+            <h1 className='navbar-item'>Tell.se</h1>
+            <a
+                role='button'
+                class='navbar-burger'
+                aria-label='menu'
+                aria-expanded='false'
+            >
+                <span aria-hidden='true' />
+                <span aria-hidden='true' />
+                <span aria-hidden='true' />
+            </a>
+        </div>
+        <div class='navbar-menu'>
+            <a className='navbar-item'>Tjänster</a>
+            <a className='navbar-item'>Tjänster</a>
+            <a className='navbar-item'>Tjänster</a>
+            <a className='navbar-item'>Tjänster</a>
+        </div>
+    </nav>
 )
 
 export default Header
