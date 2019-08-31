ffca55f81b28d67422f1812836f01544b84b538d
Jonas Olaussen <jonasolaussen@gmail.com>
2019-08-31 16:30:35 +0200

    added images

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,5 +1,28 @@
 import React from 'react'
 
+const members = [
+    {
+        id: 'oskarberggren',
+        name: 'Oskar Berggren',
+        role: 'Ekonomiansvarig'
+    },
+    {
+        id: 'jesperhedlund',
+        name: 'Jesper Hedlund',
+        role: 'Implementationsnsvarig'
+    },
+    {
+        id: 'nilsjonsson',
+        name: 'Nils Jonsson',
+        role: 'IT Ansvarig'
+    },
+    {
+        id: 'jonasolaussen',
+        name: 'Jonas Olaussen',
+        role: 'Allansvarig'
+    }
+]
+
 const AboutUs = () => (
     <section id='about-us' name='about-us' className='section is-medium'>
         <div className='container'>
@@ -19,6 +42,22 @@ const AboutUs = () => (
                 alternativ till de mer traditionella webbyråerna. Kontakta oss
                 idag för att öka ditt företags försäljning!
             </p>
+            <section className='section'>
+                <div className='columns'>
+                    {members.map(member => (
+                        <div className='column has-text-centered'>
+                            <figure className='image is-square'>
+                                <img
+                                    className='is-rounded'
+                                    src={require(`../assets/${member.id}.jpg`)}
+                                />
+                            </figure>
+                            <h3 className='title is-5'>{member.name}</h3>
+                            <h4 className='title is-6'>{member.role}</h4>
+                        </div>
+                    ))}
+                </div>
+            </section>
         </div>
     </section>
 )
