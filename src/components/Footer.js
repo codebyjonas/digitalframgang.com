d4dfd0cfc204c35f2197581c13f673a646df925a
unknown <jesper.g.hedlund@gmail.com>
2019-08-31 16:25:30 +0200

    footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,12 +4,12 @@ const Footer = () => (
     <footer className='section is-small'>
         <div className='content has-text-centered'>
             <div className='columns'>
-                <div className='column'>
-                    <p>@All copyright reserved to Jonas Olaussen</p>
-                </div>
                 <div className='column has-text-left'>
                     <strong>Our Idea</strong>
                     <p>Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. </p>
+                </div>
+                <div className='column'>
+                    <strong>@All copyright reserved to Jonas Olaussen</strong>
                 </div>
                 <div className='column'>
                     <strong>Adress</strong>
