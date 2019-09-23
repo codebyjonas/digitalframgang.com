7f52ec7ff31d5d36bd6c64a45ca0a324459d0209
Jonas Olaussen <jonasolaussen@gmail.com>
2019-09-23 13:48:24 +0200

    Updates

---
+++ precache-manifest.090f4dadf4a50feefe670fb086f35c4c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbbcdb8fffa6cf9a8e2b12d64bfce9a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b394a700830bc64e58d9",
+    "url": "/static/css/main.fe1d6255.chunk.css"
+  },
+  {
+    "revision": "7fd96b45cb6a183a378c",
+    "url": "/static/js/2.ff2927ab.chunk.js"
+  },
+  {
+    "revision": "b394a700830bc64e58d9",
+    "url": "/static/js/main.61f2a962.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2362f86ee4950db68a24b96a911fd23e",
+    "url": "/static/media/hero-image.2362f86e.jpg"
+  },
+  {
+    "revision": "652aacbb692913ab5bcfe91db4526c5e",
+    "url": "/static/media/jesperhedlund.652aacbb.jpg"
+  },
+  {
+    "revision": "93f13ff59c84daefbde63f1a1d5b9e96",
+    "url": "/static/media/jonasolaussen.93f13ff5.jpg"
+  },
+  {
+    "revision": "49a90391b247b471823c0a813d497bd6",
+    "url": "/static/media/nilsjonsson.49a90391.jpg"
+  },
+  {
+    "revision": "d5735069a7b99fded6beba7a584c3be9",
+    "url": "/static/media/oskarberggren.d5735069.jpg"
+  },
+  {
+    "revision": "0169bc2ae3760ffef9931b959a59be96",
+    "url": "/static/media/whyuswithshadow.0169bc2a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fc162a9f52f1369bc4b728b1c28e87f7.js"
+  "/precache-manifest.090f4dadf4a50feefe670fb086f35c4c.js"
 );
 
 self.addEventListener('message', (event) => {
