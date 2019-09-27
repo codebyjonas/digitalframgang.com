ad293370989b17569116fc1104c852b7bdffa84d
Jonas Olaussen <jonasolaussen@gmail.com>
2019-09-27 21:34:45 +0200

    added nice stuff to this branch

--- src/components/Why.js
+++ src/components/Why.js
@@ -1,20 +1,21 @@
 import React from 'react'
+import { MdTrackChanges, MdAttachMoney, MdHistory } from 'react-icons/md'
 
 const whyyUsTop3 = [
     {
-        icon: 'Ikon',
+        icon: <MdAttachMoney />,
         title: 'Kostnadseffektiva',
         copy:
             'Tack vare våra kostnadseffektiva metoder kan vi erbjuda ett lågt pris och fortfarande leverera en högkvalitativ tjänst.'
     },
     {
-        icon: 'Ikon',
+        icon: <MdTrackChanges />,
         title: 'Helhetslösning',
         copy:
             'Vi hjälper er från a till b så att ni kan fokusera på er kärnverksamhet. Ni väljer själva  hur inolverade ni vill vara.'
     },
     {
-        icon: 'Ikon',
+        icon: <MdHistory />,
         title: 'Lång erfarenheter',
         copy:
             'Mellan oss har vi flera års erfarenhet av både marknadsanalys och webbutveckling, därför kan vi garantera en optimerad hemsida.'
@@ -28,9 +29,11 @@ const Why = () => (
         className='section is-medium why-section'
     >
         <div id='why-text-container' className='container'>
-            <h2 className='title is-1 has-blue-sidebar'>Varför välja oss?</h2>
-            <div className='columns'>
-                <div className='column is-two-thirds'>
+            <div className='columns is-8 is-variable is-desktop'>
+                <div className='column'>
+                    <h2 className='title is-1 has-blue-sidebar'>
+                        Varför välja oss?
+                    </h2>
                     <div id='why-text'>
                         <p>
                             Vi är kostnadseffektiva webbutvecklare som riktar
@@ -51,11 +54,14 @@ const Why = () => (
                         </p>
                     </div>
                 </div>
+                <div className='column is-1'></div>
                 <div className='column top-3-wrapper'>
                     {whyyUsTop3.map(topFeature => (
-                        <div className='columns top-3-item'>
-                            <div className='column'>{topFeature.icon}</div>
-                            <div className='column'>
+                        <div className='top-3-item '>
+                            <div>
+                                <span className='icon'>{topFeature.icon}</span>
+                            </div>
+                            <div>
                                 <h4 className='title is-5'>
                                     {topFeature.title}
                                 </h4>
