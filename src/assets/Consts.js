4beec7304f085e5b3bbcbd104b5b6f6e1a642184
Jonas Olaussen <jonasolaussen@gmail.com>
2019-10-21 10:04:16 +0200

    styled up the footer

---
+++ src/assets/Consts.js
@@ -0,0 +1,18 @@
+export const links = [
+    {
+        displayName: 'Våra tjänster',
+        link: 'services'
+    },
+    {
+        displayName: 'Varför oss',
+        link: 'why-us'
+    },
+    {
+        displayName: 'Vilka är vi',
+        link: 'about-us'
+    },
+    {
+        displayName: 'Kontakta oss',
+        link: 'contact-us'
+    }
+]
