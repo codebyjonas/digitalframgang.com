7cd198a6fddb1d863717985b971f629b8226fe9e
Jonas Olaussen <jonasolaussen@gmail.com>
2019-10-23 11:51:43 +0200

    redsign of everything yes

--- src/components/Services.js
+++ src/components/Services.js
@@ -8,93 +8,47 @@ const Services = () => (
             <h2 className='title is-1 has-blue-sidebar'>
                 Vår metod för en optimerad hemsida
             </h2>
-            <div className='columns'>
-                <div className='column card-shadow'>
-                    <div className='columns is-vcentered'>
-                        <div className='column is-1 is-paddingless'>
-                            <span className='icon has-orange-light is-size-2 is-flex'>
-                                <MdAssessment />
-                            </span>
-                        </div>
-                        <div className='column'>
-                            <h2 className='title is-3'>Helhetsanalys</h2>
-                        </div>
-                    </div>
-                    <div>
-                        <p>
-                            För att garantera träffsäkerheten i våra åtgärder
-                            ser vi till att erhålla en tydlig bild av både ert
-                            företag och era målkunder
-                        </p>
-                    </div>
+            <div className='columns service-cards'>
+                <div className='column service-card'>
+                    <span className='icon'>
+                        <MdAssessment />
+                    </span>
+                    <h2 className='title is-3'>Helhetsanalys</h2>
+                    <p>
+                        För att garantera träffsäkerheten i våra åtgärder ser vi
+                        till att erhålla en tydlig bild av både ert företag och
+                        era målkunder
+                    </p>
                 </div>
-                <div className='column card-shadow'>
-                    <div className='columns is-vcentered'>
-                        <div className='column is-1 is-paddingless'>
-                            <span className='icon has-orange-light is-size-2 is-flex'>
-                                <MdSearch />
-                            </span>
-                        </div>
-                        <div className='column'>
-                            <h2 className='title is-3'>Synlighet</h2>
-                        </div>
-                    </div>
+                <div className='column service-card'>
+                    <span className='icon'>
+                        <MdSearch />
+                    </span>
+                    <h2 className='title is-3'>Synlighet</h2>
                     <p>
                         Genom effektiva punktinsatser gör vi er hemsida synlig -
                         Så att potentiella kunder hittar till er istället för
                         till konkurrenterna
                     </p>
                 </div>
-                <div className='column card-shadow'>
-                    <div className='columns is-vcentered'>
-                        <div className='column is-1 is-paddingless'>
-                            <span class='icon has-orange-light is-size-2 is-flex'>
-                                <MdLoop />
-                            </span>
-                        </div>
-                        <div className='column'>
-                            <h2 className='title is-3'>Konvertering</h2>
-                        </div>
-                    </div>
+                <div className='column service-card'>
+                    <span class='icon'>
+                        <MdLoop />
+                    </span>
+
+                    <h2 className='title is-3'>Konvertering</h2>
                     <p>
                         Vi nöjer oss inte med att enbart locka fler besökare
                         till er hemsida , vi hjälper er även att konvertera
                         dessa besökare till kunder
                     </p>
                 </div>
-            </div>
-            <div className='columns'>
-                <div className='column card-shadow'>
-                    <div className='columns is-vcentered'>
-                        <div className='column is-1 is-paddingless'>
-                            <span class='icon has-orange-light is-size-2 is-flex'>
-                                <MdAttachMoney />
-                            </span>
-                        </div>
-                        <div className='column'>
-                            <h2 className='title is-3'>Kostnadseffektivt</h2>
-                        </div>
-                    </div>
-                    <p>
-                        Vi vill att synlighet och konvertering ska vara
-                        tillgängligt för alla företag. Därför fokuserar vi på
-                        att leverera högeffektiva lösningar utan att krångla
-                        till det. Sanningen är att man når väldigt goda resultat
-                        med en relativt låg insats, förutsatt att man fokuserar
-                        på rätt saker
-                    </p>
-                </div>
-                <div className='column card-shadow'>
-                    <div className='columns is-vcentered'>
-                        <div className='column is-1 is-paddingless'>
-                            <span class='icon has-orange-light is-size-2 is-flex'>
-                                <FaMailBulk />
-                            </span>
-                        </div>
-                        <div className='column'>
-                            <h2 className='title is-3'>Uppföljning</h2>
-                        </div>
-                    </div>
+                <div className='column service-card'>
+                    <span class='icon'>
+                        <FaMailBulk />
+                    </span>
+
+                    <h2 className='title is-3'>Uppföljning</h2>
                     <p>
                         Vi håller vad vi lovar och därför är vi noggranna i vår
                         uppföljning. För att ni kontinuerligt ska kunna följa
