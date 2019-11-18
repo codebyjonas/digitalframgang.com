18ce0eed4a6038110aba3f399bb3d99d82b5a650
Jonas Olaussen <jonasolaussen@gmail.com>
2019-11-18 11:49:49 +0100

    created a new style of the reference

--- src/components/References.js
+++ src/components/References.js
@@ -10,6 +10,9 @@ const references = [
             'Digital Framgång moderniserade Tornby Biltekniks hemsida, till en konverteringsoptimerad och sökmotorsoptimerad hemsida',
         longDesc:
             'Tornby Bilteknik behövde en ny hemsida som skulle vara tydlig, enkel och modern. Förutom att utveckla en hemsida efter dessa behov hjälpte vi även dem med såväl fotografering som insamling av kundreferenser för att skapa en konverterande hemsida.',
+        clientQuote:
+            '”Vi är väldigt nöjda med samarbetet. Grabbarna på Digital Framgång är otroligt professionella samtidigt som upprätthåller hög flexibilitet under processen, något som är särskilt viktigt för oss som har fullt upp om dagarna.”',
+        clientNameAndRole: 'Fredrik Menck, Grundare',
         link: 'http://tornbybilteknik.com'
     }
 ]
@@ -22,15 +25,9 @@ const References = () => (
     >
         <div className='container'>
             <h2 className='title is-1 has-blue-sidebar'>Våra kundcase</h2>
-            <div className='columns'>
-                {references.map(reference => (
-                    <div className='column'>
-                        <Reference reference={reference}></Reference>
-                    </div>
-                ))}
-                <div className='column'></div>
-                <div className='column'></div>
-            </div>
+            {references.map(reference => (
+                <Reference reference={reference}></Reference>
+            ))}
         </div>
         <SkewedDivider color='#fff' direction='left' />
     </section>
