6a12a87b9004a4ccaea08246e2c46a53caab6ade
unknown <jesper.g.hedlund@gmail.com>
2019-12-17 17:12:21 +0100

    changed layout on references

--- src/components/References.js
+++ src/components/References.js
@@ -6,35 +6,25 @@ const references = [
     {
         id: 'tornbybilteknik',
         title: 'Tornby Bilteknik',
-        shortDesc:
-            'Digital Framgång moderniserade Tornby Biltekniks hemsida, till en konverteringsoptimerad och sökmotoroptimerad hemsida',
-        longDesc:
-            'Tornby Bilteknik behövde en ny hemsida som skulle vara tydlig, enkel och modern. Förutom att utveckla en hemsida efter dessa behov hjälpte vi även dem med såväl fotografering som insamling av kundreferenser för att skapa en konverterande hemsida.',
-        clientQuote:
-            '”Vi är väldigt nöjda med resultatet. Framförallt har grabbarna på Digital Framgång varit flexibla och lyhörda, något som är viktigt för oss som har fullt upp om dagarna.”',
+        assignment: ['Tornby Bilteknik behövde en ny hemsida som skulle vara tydlig, enkel och modern.',
+        'Personalen spenderade för mycket telefontid åt att besvara frågor som kunde hanterats via mail ',
+        'Behövde hjälp med fotografering av personal och framtagning av beskrivande bilder '],
+        clientQuote: '”Vi är väldigt nöjda med resultatet. Framförallt har grabbarna på Digital Framgång varit flexibla och lyhörda, något som är viktigt för oss som har fullt upp om dagarna.”',
         clientNameAndRole: 'Fredrik Menck, Grundare',
         link: 'http://tornbybilteknik.com',
-        results: [
-            'Sökmotoroptimerad: Förstasidan på Google för relevanta sökord',
-            'Flerdubblad konverteringsgrad från besökare till kontaktformulär, vilket frigjorde tid för personalen'
-        ]
+        results: ['Sökmotoroptimerad: Förstasidan på Google för relevanta sökord',' Flerdubblad konverteringsgrad från besökare till kontaktformulär', 'vilket frigjorde tid för personal']
     }
 ]
 
 const References = () => (
-    <section
-        id='references'
-        name='prince-info'
-        className='section is-medium has-light-grey-background has-position-relative'
-    >
+    <section id='references' name='prince-info' className='section is-medium has-light-grey-background has-position-relative'>
         <div className='container'>
             <h2 className='title is-1 has-blue-sidebar'>Våra kundcase</h2>
             {references.map(reference => (
                 <Reference reference={reference}></Reference>
             ))}
         </div>
-        <SkewedDivider color='#fff' direction='left' />
+        <SkewedDivider color='#fff' direction='left'/>
     </section>
 )
-
 export default References
--- src/components/subcomponents/Reference.js
+++ src/components/subcomponents/Reference.js
@@ -14,23 +14,30 @@ const Reference = props => (
             <h3 className='title is-4 has-blue-sidebar'>
                 {props.reference.title}
             </h3>
-            <p>{props.reference.shortDesc}</p>
-            <p>{props.reference.longDesc}</p>
-            <p className='client-quote'>{props.reference.clientQuote}</p>
-            <p className='client-name-and-role'>
-                {props.reference.clientNameAndRole}
-            </p>
+            <h4>Vårt uppdrag</h4>
+            <ul>
+                {props.reference.assignment.map((assign)=>(
+                    <li>{assign}</li>
+                ))}
+            </ul>
+            <h4>
+                Vårt resultat
+            </h4>
+            <ul>
+                {props.reference.results.map(result=>(
+                    <li>{result}</li>
+                ))}
+            </ul>
+            <div className="client-box">
+                <p className='client-quote'>{props.reference.clientQuote}</p>
+                <p className='client-name-and-role'>
+                    {props.reference.clientNameAndRole}
+                </p>
+            </div>
             <a href={props.reference.link} target='_blank'>
                 >> Gå till {props.reference.title}
             </a>
-            <div className='nice-stats'>
-                <h4 className='title is-6'>Resultat</h4>
-                <div className='columns'>
-                    {props.reference.results.map(result => (
-                        <div className='column'>{result}</div>
-                    ))}
-                </div>
-            </div>
+
         </div>
     </div>
 )
